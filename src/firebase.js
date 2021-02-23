70fc1b3c25102c3f9e7e383e077db07c7b89bac5
Zaibi27 <55849240+Zaibi27@users.noreply.github.com>
2021-02-23 16:46:12 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase" ;
 
+// paste here the firebaseConfig from the firebase SDK snippet
 const firebaseConfig = {
-    apiKey: "AIzaSyDk3XCd2ui5T9Pch3NGjNONMehkYBDYoCA",
-    authDomain: "facebook-clone-app-27.firebaseapp.com",
-    projectId: "facebook-clone-app-27",
-    storageBucket: "facebook-clone-app-27.appspot.com",
-    messagingSenderId: "161009794776",
-    appId: "1:161009794776:web:0de364816797e17ea9a1ad",
-    measurementId: "G-QNSSMNBR1F"
+    
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig) ;
